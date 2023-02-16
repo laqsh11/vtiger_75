e0a95133e9d5bf3fc2f51f3c552be26406481a2f
Laksh <vs2686050@gmail.com>
2023-02-16 12:16:49 +0530

    setup project

---
+++ config.csrf-secret.php
@@ -0,0 +1,1 @@
+<?php $secret = "88338daccee097ec139431f3f9f4b4272f5e4c8b";
---
+++ test/templates_c/v7/93ac54f46e4ef8e55a64475e721cf3c4be0aab5e_0.file.IndexPostProcess.tpl.php
@@ -0,0 +1,28 @@
+<?php
+/* Smarty version 3.1.39, created on 2023-02-16 06:44:30
+  from 'C:\xampp\htdocs\test_project\layouts\v7\modules\Vtiger\IndexPostProcess.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_63edd0ced08982_51248146',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '93ac54f46e4ef8e55a64475e721cf3c4be0aab5e' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\test_project\\layouts\\v7\\modules\\Vtiger\\IndexPostProcess.tpl',
+      1 => 1669872319,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_63edd0ced08982_51248146 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+	</div>
+</div>
+</div><?php }
+}
